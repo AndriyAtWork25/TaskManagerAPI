86a5236cecbb7b2fae060422741dc4ab65285f4c
AndriyAtWork <andriytsar3388@gmail.com>
2025-11-12 16:10:36 +0100

    Fix User model import path for Render deploy

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from '../../models/user.js';
+import User from '../models/user.js';
 import { ApiError } from '../utils/ApiError.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 
